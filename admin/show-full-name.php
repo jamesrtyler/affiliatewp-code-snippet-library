22e7923d6f544a386aa48abe88504fdbbe17886c
Andrew Munro <andrew@sumobi.com>
2016-07-28 14:33:58 +1200

    Show full name on referrals screen

--- admin/show-full-name.php
+++ admin/show-full-name.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name: AffiliateWP - Show Full Name
  * Plugin URI: https://affiliatewp.com/
- * Description: Show the affiliate's first and last name on the Affiliates screen
+ * Description: Show the affiliate's first and last name on the Affiliates & Referrals screen
  * Author: Andrew Munro, Sumobi
  * Author URI: https://affiliatewp.com
- * Version: 1.0
+ * Version: 1.0.1
  */
 
 /**
@@ -14,7 +14,7 @@
  * If no last name is present, it will show the first name
  * If no first or last name is present, it will show the display name
  */
-function affwp_custom_show_full_name( $value, $affiliate ) {
+function affwp_custom_affiliates_show_full_name( $value, $affiliate ) {
 
 	$display_name = affiliate_wp()->affiliates->get_affiliate_name( $affiliate->affiliate_id );
 	$name         = affwp_get_affiliate_name( $affiliate->affiliate_id  );
@@ -30,4 +30,23 @@ function affwp_custom_show_full_name( $value, $affiliate ) {
 	return $value;
 
 }
-add_filter( 'affwp_affiliate_table_name', 'affwp_custom_show_full_name', 10, 2 );
+add_filter( 'affwp_affiliate_table_name', 'affwp_custom_affiliates_show_full_name', 10, 2 );
+
+/**
+ * By default AffiliateWP shows the "display name" on the Affiliates -> Referrals screen
+ * This shows the affiliates first and last name, if present
+ * If no last name is present, it will show the first name
+ * If no first or last name is present, it will show the display name
+ */
+function affwp_custom_referrals_show_full_name( $value, $referral ) {
+
+	$display_name   = affiliate_wp()->affiliates->get_affiliate_name( $referral->affiliate_id );
+	$name           = affwp_get_affiliate_name( $referral->affiliate_id  );
+	$affiliate_name = $name ? $name : $display_name;
+
+	$value = apply_filters( 'affwp_referral_affiliate_column', '<a href="' . admin_url( 'admin.php?page=affiliate-wp-referrals&affiliate_id=' . $referral->affiliate_id ) . '">' . $affiliate_name . '</a>', $referral );
+
+	return $value;
+
+}
+add_filter( 'affwp_referral_table_affiliate', 'affwp_custom_referrals_show_full_name', 10, 2 );
