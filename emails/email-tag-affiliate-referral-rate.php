6afb47a4f91fbaf229d402411884e9109f087b04
Andrew Munro <andrew@sumobi.com>
2016-08-01 19:51:34 +1200

    Affiliate Referral Rate Email Tag

---
+++ emails/email-tag-affiliate-referral-rate.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Affiliate Referral Rate Email Tag
+ * Plugin URI: https://affiliatewp.com
+ * Description: Adds an {affiliate_referral_rate} email tag that displays the afffiliate their commission rate
+ * Author: Andrew Munro
+ * Author URI: https://affiliatewp.com
+ * Version: 1.0
+ */
+
+/**
+ * Add a new {affiliate_referral_rate} email tag
+ */
+function affwp_custom_email_tag_affiliate_referral_rate( $email_tags, $this ) {
+
+	$email_tags['affiliate_referral_rate'] = array(
+		'tag'         => 'affiliate_referral_rate',
+		'description' => __( 'The affiliate\'s referral rate', 'affiliate-wp' ),
+		'function'    => 'affwp_email_tag_affiliate_referral_rate'
+	);
+
+	return $email_tags;
+}
+add_filter( 'affwp_email_tags', 'affwp_custom_email_tag_affiliate_referral_rate', 10, 2 );
+
+/**
+ * Return the affiliate rate
+ */
+function affwp_email_tag_affiliate_referral_rate( $affiliate_id = 0 ) {
+	return affwp_get_affiliate_rate( $affiliate_id, true );
+}
